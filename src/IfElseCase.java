b496128a51db633a8e26909efacb1e501d1a487f
Hang Tran <hangtt29@onemount.com>
2021-05-13 11:11:47 +0700

    chinh sua file

--- src/IfElseCase.java
+++ src/IfElseCase.java
@@ -4,10 +4,10 @@ public class IfElseCase {
             System.out.println("Là tháng có 30 ngày");
         } else if (month == 2) {
             System.out.println("Là tháng có 28 hoặc 29 ngày");
-        } else if (month>= 13){
-            System.out.println("Không có tháng nào đâu bạn");
-        }else {
+        } else if (month == 1 || month == 3 || month == 5 || month == 7 || month ==8 || month == 10 || month == 12 ){
             System.out.println("Là các tháng có 31 ngày");
+        }else {
+            System.out.println("Không có tháng nào đâu bạn");
         }
 
     }
