cea04f6e465744ac311571bc2cdde4c349d92d9d
Hang Tran <hangtt29@onemount.com>
2021-05-16 17:41:18 +0700

    add file bài tập về nhà buổi 6

---
+++ Bài tập buổi 6/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        DemChuoi d = new DemChuoi();
+        //Bài 5+ 15
+        System.out.println("You only live once, but if you do it right, once is enough");
+        d.inDexChuoi();
+
+        //Bài 8
+        d.demChuoi();
+
+
+    }
+
+
+}
---
+++ Bài tập buổi 6/DemChuoi.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DemChuoi {
+
+
+    static void inDexChuoi() {
+        String str = "You only live once, but if you do it right, once is enough";
+        System.out.println("In ra index của tất cả ký tự  o trong chuỗi trên: ");
+        int dem = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.toString(str.charAt(i)).equals("o")) {
+                System.out.print(i + "\t");
+                dem++;
+            }
+        }
+        System.out.println("\nTổng số lần xuất hiện trong chuỗi:" + dem);
+    }
+
+    static void demChuoi() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Nhập vào 1 chuỗi:");
+        String ip = s.nextLine();
+        ip = ip.trim();
+        int dem = 0;
+        for (int i = 0; i < ip.length(); i++) {
+            if (Character.toString(ip.charAt(i)).equals(" ")) {
+                dem++;
+            }
+        }
+        dem = dem + 1;
+        System.out.println("In tổng số từ của chuỗi " + dem);
+    }
+
+}
